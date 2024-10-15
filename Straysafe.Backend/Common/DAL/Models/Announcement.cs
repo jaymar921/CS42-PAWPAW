ec5f5a926250424ebb4ec0036b8054e1511027b5
jaymar921 <jayharronabejar@gmail.com>
2024-10-15 20:31:09 +0800

    Added Models for Announcements

---
+++ Straysafe.Backend/Common/DAL/Models/Announcement.cs
@@ -0,0 +1,12 @@
+﻿namespace Straysafe.Backend.Common.DAL.Models
+{
+    public class Announcement
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public string Attachment { get; set; } = string.Empty;
+        public DateTime Date { get; set; } = DateTime.Now;
+        public string PostedBy { get; set; } = string.Empty;
+    }
+}
---
+++ Straysafe.Backend/Common/DAL/Models/AnnouncementMetadata.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Straysafe.Backend.Common.DAL.Models
+{
+    public class AnnouncementMetadata
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; } 
+        public Guid AnnouncementId { get; set; }
+        public Guid User {  get; set; }
+        public bool Liked { get; set; } = false;
+        public bool Seen { get; set; } = false;
+        public string Comments { get; set; } = string.Empty;
+    }
+}
