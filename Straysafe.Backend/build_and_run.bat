61ab5f09648791a4ffc685378995b9d374507335
jaymar921 <jayharronabejar@gmail.com>
2024-11-07 20:57:43 +0800

    Project Production Starter added

---
+++ Straysafe.Backend/build_and_run.bat
@@ -0,0 +1,1 @@
+dotnet run Straysafe.Backend.csproj
--- start_app.bat
+++ start_app.bat
@@ -1,25 +1,21 @@
 @echo off
-color 0A
-echo [STRAY SAFE] - CS-42 - PAWPAW
-echo Starting App
-echo Initializing...
+echo.
+echo [Stray Safe]
+echo Project Requirements:
+echo.
+echo - Dotnet SDK installed
+echo - MSSQL installed (https://www.microsoft.com/en-us/sql-server/sql-server-downloads)
+echo - NPM installed
+echo - Firewall Ports exposed (5173, 5077)
+echo.
+echo (Make sure that the requirements are met to avoid issues)
+echo.
+echo.
 timeout /t 5 /nobreak > NUL
-if exist "C:\Program Files\Docker\Docker\Docker Desktop.exe" (
-  echo Starting Docker Desktop Service
-  "C:\Program Files\Docker\Docker\Docker Desktop.exe"
-  timeout /t 10 /nobreak > NUL
-) else (
-  echo Please install Docker Desktop
-  echo https://docs.docker.com/desktop/install/windows-install/
-  pause
-  exit
-)
-cd "WebApp"
-start cmd /k "docker-compose -f docker-compose.yml up --build"
-echo Starting Services... 
-echo.
-echo.
-echo [Open Links]
-echo Strayver Frontend - http://localhost:5173
-echo Strayver Backend - http://localhost:8080/swagger
-pause
+echo Running the Project....
+timeout /t 5 /nobreak > NUL
+cd Straysafe.Backend
+start build_and_run.bat
+cd ..
+cd Straysafe.Frontend
+npm run dev
